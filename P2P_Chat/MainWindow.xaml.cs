8ae9c12fca34db20ae9e1d6eaedcae7177dce302
mabudzynska <mabudzynska00@gmail.com>
2026-03-30 20:42:59 +0200

    Init Commit and UDP Sender / Receiver

---
+++ P2P_Chat/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// Moja bibliotek z plikami netowrk
+using P2P_Chat.Network;
+using System.Threading;
+namespace P2P_Chat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UdpListener listener = new UdpListener(54321);
+            Thread thread_UdpListener = new Thread(() => listener.StartListening(12345));
+
+
+            UdpBroadcaster broadcaster = new UdpBroadcaster(12345);
+
+            while (true)
+            {
+                string message = "DUPA 123";
+                broadcaster.sendBroadcast(message, 54321);
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
---
+++ P2P_Chat/Network/UdpBroadcaster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+// System.net for IPAddress and IPEndPoint
+using System.Net;
+// Socket for UDP communication
+using System.Net.Sockets;
+
+
+namespace P2P_Chat.Network
+{
+    class UdpBroadcaster
+    {
+        private UdpClient udpClient;
+        int port;
+        public UdpBroadcaster(int port)
+        {
+            this.port = port;
+            udpClient = new UdpClient(port);
+            udpClient.EnableBroadcast = true;
+        }
+
+        public void sendBroadcast(string message, int port)
+        {
+            // ep ma informacje o Adresie docelowym oraz porcie docelowym
+            IPEndPoint ep = new IPEndPoint(IPAddress.Broadcast, 54321);
+
+            // zamiana stringa na tablice bajtów (UDP wysyła ramkę bajtów). Dekoduje zgodznie z UTF8
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            // Wyślij ramkę danych po UDP do wszystkich urządzeń w sieci.
+            udpClient.Send(data, data.Length, ep);
+
+
+        }
+    }
+}
