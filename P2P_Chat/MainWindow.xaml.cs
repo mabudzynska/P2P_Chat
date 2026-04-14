15a3e9540b58fe113bce5a3a9a92402977984dfd
mabudzynska <mabudzynska00@gmail.com>
2026-04-14 20:55:32 +0200

    merged with Piotr's GUI part. Added TCP P2P Communication

--- P2P_Chat/MainWindow.xaml.cs
+++ P2P_Chat/MainWindow.xaml.cs
@@ -20,39 +20,35 @@ namespace P2P_Chat
 {
     public partial class MainWindow : Window
     {
+        // Serwic do obsługi UDP
         private UdpService udp;
+        // Serwic do obsługi TCP
+        private TcpService tcp;
+
+        // Obiekt do: Wykrywanie peerów, wysyłanie wiadomości typu HELLO / GOODBYE
         private DiscoveryService discovery;
+        // Menadżer zarządzający peerami
         private PeerManager peerManager;
-        private void OnUdpMessage(string msg, IPEndPoint endpoint)
-        {
-            Model? model = Parser.ParseJsonToModel(msg);
-
-            if (model == null)
-                return;
-
-            if (model.Type == MessageType.HELLO)
-            {
-                discovery.HandleHello(model, endpoint);
-            }
-            else if (model.Type == MessageType.GOODBYE)
-            {
-                discovery.HandleBye(model, endpoint);
-            }
-        }
+        
         public MainWindow()
         {
             InitializeComponent();
-
+            // Utworzenie clienta TCP na porcie 53241
+            tcp = new TcpService(53241);
+            // Utworzenie socketa UDP na porcie 50000
             udp = new UdpService(50000);
+            // Konstruktor menadżera peerów
             peerManager = new PeerManager();
+            // Inicjalizacja serwisu zarządzania statusem w sieci.
             discovery = new DiscoveryService(
-                udp,
-                "Marta",
-                53241,
-                peerManager
+                udp,        // handler do serwisu UDP
+                "Marta",    // NAZWA UŻYTKOWNIKA - ustwić dopiero po jakimś okienku logowania / popupie
+                53241,      // PORT TAKI SAM JAK W TCP!!!
+                peerManager // handler do menadżera peerów
             );
 
-            
+            // Dodanie eventów do odbierania wiadomości po TCP i UDP
+            tcp.OnMessageReceived += OnTcpMessage;
             udp.OnMessageReceived += OnUdpMessage;
             Loaded += MainWindow_Loaded;
         }
@@ -64,19 +60,98 @@ namespace P2P_Chat
 
         private void StartNetworking()
         {
-            // 📥 Listener (działa w tle)
-            _ = Task.Run(() => udp.StartListening());
+            // Uruchomienie funkcji asynchronicznych do nasłuchiwania po TCP i UDP (w tle)
+            _ = udp.StartListening();
+            _ = tcp.StartListening();
 
-            // 📤 Broadcast loop (też w tle)
+            // Broadcast loop działający w tle. Podtrzymanie statusu w sieci oraz walidacja pozostałych peerów.
             _ = Task.Run(async () =>
             {
                 while (true)
                 {
+                    // co 3 sekundy wyślij że jesteś aktywny, a potem sprawdź czy pozostałe peery nie zrobiły timeoutu.
                     await discovery.SendHello();
                     peerManager.ValidatePeers();
                     await Task.Delay(3000);
                 }
             });
+        }
+
+        // Calback dla btn click
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            // pobieranie wiadomości z pola
+            string msg = MessageInput.Text;
+            // wyślij tylko jeśli pole nie jest puste!
+            if (!string.IsNullOrEmpty(msg))
+            {
+                // Konwertowanie wiadomości do formatu JSON
+                var model = new Model
+                {
+                    Type = MessageType.MESSAGE,
+                    Name = "Marta",
+                    payload = msg
+                };
+                string json = Parser.ParseModelToJson(model);
+
+                /* UWAGA! Na ten moment to działa troche jak broadcast (dla testu) - wysyła do każdego wykrytego peera w sieci
+                 napisaną wiadomość - Prawdopodobnie wyśle też do samego siebie - nastąpi zapętlenie!!!!! Docelowo należy to zmienić
+                , gdy zostanie wybrany użytkownik do którego piszemy, wysłać do konkretnego peera po kluczu. 
+                
+                 ! Klase Model możliwe że też trzeba zmienić żeby przechowywała Nazwę użytkownika. */
+                foreach (var peer in peerManager.GetPeers())
+                {
+                    // wyślij na każdy dostępny peer
+                    await tcp.SendMessage(json, peer.IP, peer.Port);
+                }
+                // wyświetl moją wiadomość
+                ChatBox.Items.Add($"ME: {msg}");
+                // wyczyść pole input
+                MessageInput.Clear();
+            }
+        }
+
+        // Callback dla odebranej wiadomości po UDP
+        private void OnUdpMessage(string msg, IPEndPoint endpoint)
+        {
+            
+            Model? model = Parser.ParseJsonToModel(msg);
+
+            // może być null ("?"), więc trzeba sprawdzić
+            if (model == null)
+                return;
+
+            // Obsługa danych ze względu na typ otrzymanej ramki
+            if (model.Type == MessageType.HELLO)
+            {
+                discovery.HandleHello(model, endpoint);
+            }
+            else if (model.Type == MessageType.GOODBYE)
+            {
+                discovery.HandleBye(model, endpoint);
+            }
+        }
+
+        // Callback dla odebranej wiadomości po TCP
+        private void OnTcpMessage(string msg, IPEndPoint endpoint)
+        {
+            // Parsowanie json stringa do obiektu klasy Model, żeby łatwiej było obsługiwać dane
+            var model = Parser.ParseJsonToModel(msg);
+            // obsłuż wyjątek
+            if (model == null) return;
+            
+            // Tylko jeśli odebrana ramka jest typu MESSAGE
+            if (model.Type == MessageType.MESSAGE)
+            {
+                // Musimy użyć Dispatchera, bo wiadomość przychodzi z innego wątku!
+                Dispatcher.Invoke(() =>
+                {
+                    // printuj w czatboxie po nazwie.
+                    ChatBox.Items.Add($"{model.Name}: {model.payload}");
+                });
+                // logging w terminalu na wszelki wypadek
+                Console.WriteLine($"{model.Name}: {model.payload}");
+            }
         }
     }
 }
---
+++ P2P_Chat/Network/TcpService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace P2P_Chat.Network
+{
+    /* Klasa działająca na stosie TCP, odpowiadająca za komunikację P2P, wysyła dane w formacie Json na konkretny port, konkretnego adresu
+     * ip. Odbiera Dane na tym samym porcie od pozostałych użytkowników w sieci. */
+    internal class TcpService
+    {
+        // Handler do nasłuchiwania i wysyłania wiadomości TCP
+        private TcpListener listener;
+        // Port nadawczo-odbiorczy
+        private int port;
+
+        // Handler otrzymania wiadomości TCP
+        public Action<string, IPEndPoint>? OnMessageReceived;
+
+        // Inicjalizacja
+        public TcpService(int port)
+        {
+            this.port = port;
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        // Task asynchroniczny do nasłuchwiania wiadomości w sieci od pozostałych peerów.
+        public async Task StartListening()
+        {
+            // Enable listening for incoming TCP connections
+            listener.Start();
+            // logging
+            Console.WriteLine($"TCP listening on {port}");
+
+            while (true)
+            {
+                // czekaj na wiadomość
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                // obsłuż ją w tle
+                _ = HandleClient(client);
+            }
+        }
+        // Handler do obsługi wiadomości TCP
+        private async Task HandleClient(TcpClient client)
+        {
+            // alokacja bufora (max 1kb danych)
+            var stream = client.GetStream();
+            byte [] buffer = new byte[1024];
+
+            // kopiowanie danych do bufora i zapis odczytanych bajtów
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            IPEndPoint remote = (IPEndPoint)client.Client.RemoteEndPoint;
+
+            OnMessageReceived?.Invoke(message, remote);
+            client.Close();
+        }
+
+        public async Task SendMessage(string message, string ip, int port)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                await client.ConnectAsync(IPAddress.Parse(ip), port);
+                var stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(data, 0, data.Length);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending TCP message: {ex.Message}");
+            }
+        }
+    }
+}
